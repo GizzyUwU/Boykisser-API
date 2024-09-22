d14a0a7faa39eebd4f9c4cc3ba5ffb820c625df3
Gizzy <nerop455@gmail.com>
2024-09-22 16:03:42 +0100

    new media file + mov support added

--- src/pages/api/random.js
+++ src/pages/api/random.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
       if (mediaType) {
         switch (mediaType.toLowerCase()) {
           case 'video':
-            files = files.filter(file => file.endsWith('.mp4'));
+            files = files.filter(file => file.endsWith('.mp4') || file.endsWith('.mov'));
             break;
           case 'gif':
             files = files.filter(file => file.endsWith('.gif'));
